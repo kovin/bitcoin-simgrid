4acfbd60c2162090a50c84dd83d0adfb2a48f75f
Kevin Piotrkowski <kevin.piotrkowski@avature.net>
2018-02-25 19:21:29 -0300

    Performance improvements for the monitor

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -6,17 +6,17 @@ XBT_LOG_EXTERNAL_DEFAULT_CATEGORY(bitcoin_simgrid);
 void Monitor::operator()()
 {
   XBT_INFO("\nTime\t\tTotal network bytes produced");
-  long last_bytes_monitored = Node::network_bytes_produced;
+  long last_bytes_monitored = 0;
   while (Node::active_nodes > 0) {
-    if (last_bytes_monitored != Node::network_bytes_produced) {
-        last_bytes_monitored = Node::network_bytes_produced;
-        XBT_INFO(
-          "%f\t%ld\n",
-          simgrid::s4u::Engine::getClock(),
-          Node::network_bytes_produced
-        );
+    if (last_bytes_monitored == Node::network_bytes_produced) {
+      simgrid::s4u::this_actor::sleep_for(1);
+    } else {
+      last_bytes_monitored = Node::network_bytes_produced;
+      XBT_INFO(
+        "%f\t%ld",
+        simgrid::s4u::Engine::getClock(),
+        Node::network_bytes_produced
+      );
     }
-    simgrid::s4u::this_actor::sleep_for(1);
   }
-  XBT_INFO("blockchain data: %d", blockchain_data);
 }
