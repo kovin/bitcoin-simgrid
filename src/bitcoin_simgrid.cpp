746ab6a6747c0a44d99fa0abaef343de22e890e1
Kevin Piotrkowski <kevin.piotrkowski@avature.net>
2018-03-19 00:52:13 -0300

    Allow to set the simulation duration time in the "bitcoin-simgrid" binary

--- src/bitcoin_simgrid.cpp
+++ src/bitcoin_simgrid.cpp
@@ -1,24 +1,38 @@
 #include "bitcoin_simgrid.hpp"
 #include "node.hpp"
 #include "miner.hpp"
-#include "monitor.hpp"
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(bitcoin_simgrid, "bitcoing-simgrid logs");
 
+// This is the directory where the nodes should go to look for their bootstrapping data
 std::string deployment_directory;
+// By default we are goind to simulate an hour, but this can be changes using the --simulation-duration argument
+unsigned int SIMULATION_DURATION = 3600;
+
+void parse_and_validate_args(int argc, char *argv[]) {
+  xbt_assert(argc <= 6, "Usage: %s platform_file deployment_directory [-debug] [--simulation-duration <seconds>]", argv[0]);
+  if (argc > 3) {
+    for (int i = 3; i < argc; ++i) {
+      if (std::string(argv[i]) == "-debug") {
+        xbt_log_control_set("bitcoin_simgrid.fmt:%10h:%e%m%n bitcoin_simgrid.thres:debug");
+      } else if (std::string(argv[i]) == "--simulation-duration") {
+        xbt_assert(argc > i, "Missing argument for --simulation-duration");
+        ++i;
+        SIMULATION_DURATION = std::stoi(argv[i]);
+      } else {
+        xbt_assert(false, "Unknown argument '%s'", argv[i]);
+      }
+    }
+  }
+}
 
 int main(int argc, char *argv[])
 {
   simgrid::s4u::Engine e(&argc, argv);
-  argc -= 1;
-  xbt_assert(argc <= 3, "Usage: %s platform_file deployment_directory [-debug]", argv[0]);
-  if (argc == 3 && std::string(argv[3]) == "-debug") {
-    xbt_log_control_set("bitcoin_simgrid.fmt:%10h:%e%m%n bitcoin_simgrid.thres:debug");
-  }
-  srand(1);
+  parse_and_validate_args(argc, argv);
+  srand(1);// Use a constant seed to have deterministic runs on our simulations
   e.registerFunction<Node>("node");
   e.registerFunction<Miner>("miner");
-  e.registerFunction<Monitor>("monitor");
   e.loadPlatform(argv[1]);
   deployment_directory = std::string(argv[2]);
   std::string deployment_file = deployment_directory + std::string("/deployment.xml");
--- src/bitcoin_simgrid.hpp
+++ src/bitcoin_simgrid.hpp
@@ -2,6 +2,10 @@
 #define BITCOIN_SIMGRID_HPP
 #include <string>
 
+// This is the directory where the nodes should go to look for their bootstrapping data
 extern std::string deployment_directory;
+
+// This it the total time in seconds we are goind to allow the simulation to run
+extern unsigned int SIMULATION_DURATION;
 
 #endif /* BITCOIN_SIMGRID_HPP */
