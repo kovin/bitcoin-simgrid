bafc1d84999d2f06f4ab5223e2ad2f596ec9ba52
Kevin Piotrkowski <kevin.piotrkowski@avature.net>
2018-03-30 00:55:12 -0300

    Add support to intesect maps (it could be handy in the future)

--- src/aux_functions.hpp
+++ src/aux_functions.hpp
@@ -31,21 +31,40 @@ std::map<KeyType, Value> DiffMaps(const std::map<KeyType, Value> & left, const s
 template<typename KeyType, typename Value>
 std::map<KeyType, Value> JoinMaps(const std::map<KeyType, Value> & left, const std::map<KeyType, Value> & right)
 {
-    std::map<KeyType, Value> result;
-    typename std::map<KeyType, Value>::const_iterator il = left.begin();
-    typename std::map<KeyType, Value>::const_iterator ir = right.begin();
-    while (il != left.end() || ir != right.end())
-    {
-      if (il != left.end()) {
-        result.insert(std::make_pair(il->first, il->second));
-        ++il;
-      }
-      if (ir != right.end()) {
-        result.insert(std::make_pair(ir->first, ir->second));
-        ++ir;
-      }
+  std::map<KeyType, Value> result;
+  typename std::map<KeyType, Value>::const_iterator il = left.begin();
+  typename std::map<KeyType, Value>::const_iterator ir = right.begin();
+  while (il != left.end() || ir != right.end())
+  {
+    if (il != left.end()) {
+      result.insert(std::make_pair(il->first, il->second));
+      ++il;
     }
-    return result;
+    if (ir != right.end()) {
+      result.insert(std::make_pair(ir->first, ir->second));
+      ++ir;
+    }
+  }
+  return result;
+}
+
+template<typename KeyType, typename Value>
+std::map<KeyType, Value> InsersectMaps(const std::map<KeyType, Value> & left, const std::map<KeyType, Value> & right)
+{
+  std::map<KeyType, Value> result;
+  typename std::map<KeyType, Value>::const_iterator il = left.begin();
+  typename std::map<KeyType, Value>::const_iterator ir = right.begin();
+  while (il != left.end() && ir != right.end())
+  {
+    if (il->first == ir->first) {
+      result.insert(std::make_pair(il->first, il->second));
+    } else if (il->first < ir->first) {
+      ++il;
+    } else {
+      ++ir;
+    }
+  }
+  return result;
 }
 
 #endif /* AUX_FUNCTIONS_HPP */
